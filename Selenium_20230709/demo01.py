4957d698038e58a2721c6e53165592fa3ce3583a
ATHZoA79 <106025017anthonyhsu@gmail.com>
2023-07-09 16:19:04 +0800

    selenium0709

--- Selenium_20230709/demo01.py
+++ Selenium_20230709/demo01.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.chrome_executable_path="C:/Users/USER/Documents/Anthony_Hsu/Python/Selenium_20230709/chromedriver.exe"
+options.chrome_executable_path="C:/Anthony/Selenium_20230709/chromedriver.exe"
 driver = webdriver.Chrome(options=options)
 
 driver.get('http://127.0.0.1/anthony/demo01.html')
