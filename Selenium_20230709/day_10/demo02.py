db64effdd41a4b1c59161d568886043382af9453
ATHZoA79 <106025017anthonyhsu@gmail.com>
2023-07-23 16:01:11 +0800

    07231601

--- Selenium_20230709/day_10/demo02.py
+++ Selenium_20230709/day_10/demo02.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup as bs
 import requests as req
 
 url = 'https://www.mobile01.com/'
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82",
+}
 
 # req.get 是回傳doc格式資料 需要用text轉換成字串
 # 若要加上header只要在url後方加上headers屬性即可
-with req.get(url) as web:
+with req.get(url, headers=header) as web:
     soup = bs(web.text, "html.parser")
-    
-# print(data.prettify())
 
-title = soup.find('header')
-print(soup)
+header = soup.find('header')
+head = soup.find('head')
+print(head.prettify())
